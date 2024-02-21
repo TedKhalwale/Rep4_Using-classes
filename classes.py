f3a550ecbfe9e495e036bf0b55b3610e536414b3
Ted Khalwale <khalwaleted@gmail.com>
2024-02-21 23:35:12 +0300

    Initial commit

---
+++ classes.py
@@ -0,0 +1,45 @@
+# Using classes to calculate the area of a rectangle, circle, square and the 
+# volume of a sphere
+pi = 3.142
+class  area_rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+    def area(self):
+        area = self.length * self.width
+        return area
+    
+class area_circle:
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        area = pi * self.radius ** 2
+        return area
+    
+class area_square:
+    def __init__(self, side):
+        self.side = side
+    def area(self):
+        area = self.side ** 2
+        return area
+    
+class volume_sphere:
+    def __init__(self, radius):
+        self.radius = radius
+    def volume(self):
+        volume = (4 / 3) * pi * self.radius ** 3
+        return volume
+    
+# Instantiating
+
+rectangle = area_rectangle(10, 7)
+print("The area of the rectangle is: ", rectangle.area())
+
+circle = area_circle(14)
+print("The area of the circle is: ", circle.area())
+
+square = area_square(5)
+print("This is the area of the square: ", square.area())
+
+sphere = volume_sphere(7)
+print("The area of the sphere is: ", sphere.volume())
